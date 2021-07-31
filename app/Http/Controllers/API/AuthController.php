e73ca39db23bc9b48b5234f45421d9449318e837
prodavies <39768603+prodavies@users.noreply.github.com>
2021-07-31 18:30:22 +0300

    second commit

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Support\Facades\Http;
 
 class AuthController extends Controller
 {
@@ -27,10 +28,14 @@ class AuthController extends Controller
         $validated_credentials = $request->validate(['email'=>'required|email','password'=>'required']);
 
         if(!auth()->attempt($validated_credentials)){
+            //post the credentials to risk management portal
+            Http::post('localhost:3000/api/check_risk',['email'=>$validated_credentials['email']]);
             return response()->json(['alert'=>'Invalid credentials']);
         }
 
         $accessToken = auth()->user()->createToken('authToken')->accessToken;
+        //post the credentials to risk management portal
+        Http::post('localhost:3000/api/check_risk',['email'=>$validated_credentials['email']]);
         return response()->json(['user'=>auth()->user(), 'access_token'=> $accessToken],200);
     }
 }
